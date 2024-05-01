0503d7f5c3f2f6e3eee2279d6ccb2c9938a5a04b
zerj9 <96551236+zerj9@users.noreply.github.com>
2024-05-01 21:10:24 +0100

    update map

--- ui/src/app/map/page.tsx
+++ ui/src/app/map/page.tsx
@@ -1,10 +1,14 @@
 'use client'
 
 import React, { useState } from 'react';
+import { renderToStaticMarkup } from 'react-dom/server';
 import DeckGL from '@deck.gl/react';
 import { TileLayer } from '@deck.gl/geo-layers';
-import { BitmapLayer, LineLayer } from '@deck.gl/layers';
-import { MapViewState } from '@deck.gl/core';
+import { BitmapLayer, IconLayer, LineLayer } from '@deck.gl/layers';
+import { MapViewState, PickingInfo } from '@deck.gl/core';
+import { Icon } from '@mui/material';
+import PlaceIcon from '@mui/icons-material/Place';
+
 
 const INITIAL_VIEW_STATE: MapViewState = {
   longitude: -2.5, // Centered on the UK
@@ -17,6 +21,8 @@ const INITIAL_VIEW_STATE: MapViewState = {
 const MapPage = () => {
   const [selectedFilter, setSelectedFilter] = useState('');
   const [viewState, setViewState] = useState<any>(INITIAL_VIEW_STATE);
+  const [clickedPositions, setClickedPositions] = useState<[number, number][]>([]);
+  const [hoveredIconIndex, setHoveredIconIndex] = useState<number | null>(null);
 
   // Sample data for the map
   const data = [
@@ -51,14 +57,57 @@ const MapPage = () => {
       getColor: [0, 128, 255],
       getWidth: 2,
     }),
+    new IconLayer({
+      id: 'icon-layer',
+      data: clickedPositions.map((position, index) => ({ position, index })),
+      getIcon: () => ({
+        url: '/locationpin.png',
+        width: 64,
+        height: 64,
+        anchorX: 32,
+        anchorY: 64,
+      }),
+      getPosition: (d) => d.position,
+      getSize: (d) => (d.index === hoveredIconIndex ? 52 : 40),
+      sizeScale: 1,
+      pickable: true,
+      autoHighlight: true,
+      onHover: (info) => {
+        if (info.object) {
+          setHoveredIconIndex(info.object.index);
+        } else {
+          setHoveredIconIndex(null);
+        }
+      },
+    }),
   ];
 
   const handleFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedFilter(event.target.value);
   };
+ 
+  const handleClickLocation = (info: PickingInfo) => {
+    if (info.layer && info.layer.id === 'icon-layer') {
+      if (info.object) {
+        const clickedIconIndex = info.object.index;
+        setClickedPositions((prevPositions) =>
+          prevPositions.filter((_, index) => index !== clickedIconIndex)
+        );
+      }
+    } else if (info.coordinate) {
+      setClickedPositions((prevPositions) => {
+        if (info.coordinate && info.coordinate.length >= 2) {
+          const [longitude, latitude] = info.coordinate;
+          return [...prevPositions, [longitude, latitude] as [number, number]];
+        }
+        return prevPositions;
+      });
+    }
+  };
 
   const handleResetMap = () => {
     setViewState(INITIAL_VIEW_STATE);
+    setClickedPositions([]);
   };
 
   return (
@@ -69,6 +118,8 @@ const MapPage = () => {
           onViewStateChange={({ viewState }) => setViewState(viewState)}
           controller={true}
           layers={layers}
+          onClick={handleClickLocation}
+          getTooltip={({ object }) => object && `Pot of gold: ${object.position}`}
         />
         <button
           className="absolute top-4 right-4 bg-white text-black px-4 py-2 rounded-md shadow-md"
